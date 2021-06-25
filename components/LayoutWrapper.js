6567607e05153746fa48450b3850bac1ad52b387
joshuanwafor01 <joshuanwafor01@gmail.com>
2021-06-25 23:27:18 +0100

    ok

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -18,8 +18,7 @@ const LayoutWrapper = ({ children }) => {
                 <div className="mr-3">{/* <Logo /> */}</div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {/* {siteMetadata.headerTitle} */}
-                    Happy Birthay Daddy.
+                    {siteMetadata.headerTitle}
                   </div>
                 ) : (
                   siteMetadata.headerTitle
